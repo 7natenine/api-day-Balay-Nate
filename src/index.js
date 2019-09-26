6a7ade0b7292202655280b2b1274ec66a6b6e6da
balayaydemir <balayaydemir@gmail.com>
2019-09-26 10:59:37 -0700

    final commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
+import 'normalize.css';
+import './index.css';
+
 import $ from 'jquery';
 import shoppingList from './shopping-list';
-import './styles/index.css';
+
 
 const main = function () {
   shoppingList.bindEventListeners();
